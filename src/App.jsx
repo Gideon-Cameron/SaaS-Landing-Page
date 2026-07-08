e5b6e42fc27d17bc965c8ebca14d8fb8d6084a0c
Gideon Cameron <gideonwork20@gmail.com>
2026-07-08 05:30:09 -0700

    Cleaned up the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,19 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-// import Features from "./Components/Features"
 import Dashboard from "./Components/Dashboard";
 import Pricing from "./Components/Pricing";
 import Contact from "./Components/Contact";
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-slate-50">
+    <div className="min-h-screen bg-slate-50">
       <Navbar />
 
       <main>
         <Hero />
-        
-
-        {/* Placeholder Sections */}
-
-        {/* <Features
-          id="features"
-          className="min-h-screen"
-        /> */}
-
-        <Dashboard
-         id="dashboard"
-         className=""
-         />
-
-        <Pricing
-          id="pricing"
-          className="min-h-screen"
-        />
-
-        <Contact
-          id="contact"
-          className="min-h-screen"
-        />
-
-        
+        <Dashboard />
+        <Pricing />
+        <Contact />
       </main>
     </div>
   );
